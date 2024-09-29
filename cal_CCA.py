47eeb04f83b1c63d4be9d964851a4a26d28fc451
dry86 <1061239880@qq.com>
2024-09-29 11:56:52 +0800

    modified load model

--- cal_CCA.py
+++ cal_CCA.py
@@ -1,11 +1,8 @@
 import torch
-from getHiddenStates import load_model, tokens_get_hidden_states
+from getHiddenStates import load_hidden_states, tokens_get_hidden_states
 import numpy as np
-import jsonlines
 from example import cca_core
 from tqdm import tqdm
-# 设置打印选项来显示所有元素
-# torch.set_printoptions(threshold=torch.inf)
 
 def cca_decomp(A, B, device):
     """Computes CCA vectors, correlations, and transformed matrices
@@ -52,14 +49,12 @@ def cca_decomp(A, B, device):
     transformed_b = (vh @ (evecs_b @ torch.diag(inv_b) @ evecs_b.T) @ B).T
     return u, s, vh, transformed_a, transformed_b
 
-
 def mean_sq_cca_corr(rho):
     """Compute mean squared CCA correlation
     :param rho: canonical correlation coefficients returned by cca_decomp(A,B)
     """
     # len(rho) is min(A.shape[0], B.shape[0])
     return torch.sum(rho * rho) / len(rho)
-
 
 def mean_cca_corr(rho):
     """Compute mean CCA correlation
@@ -83,54 +78,13 @@ def calculate_cca(acts1, acts2, idx):
     pwcca_mean, w, _ = cca_core.compute_pwcca(results, acts1, acts2)
     print("\tPWCCA similarity: ", pwcca_mean)
 
-def pad_to_max_length(tensor_list, tokenizer):
-    # 获取所有张量的最大长度
-    max_length = max(tensor.shape[1] for tensor in tensor_list)
-    
-    # 对每个张量进行填充，使它们具有相同的长度
-    padded_tensors = [torch.nn.functional.pad(tensor, (0, 0, 0, max_length - tensor.shape[1])) for tensor in tensor_list]   # , value=tokenizer.eos_token_id
-    
-    return torch.stack(padded_tensors)
 
-def main(model1_path, model2_path, data_file_path, device1, device2):
+def main(model1_path, model2_path):
     """主函数：加载模型、读取数据、计算CCA相似性"""
-    # 加载模型和tokenizer
-    model1, tokenizer1 = load_model(model1_path, device1)
-    model2, tokenizer2 = load_model(model2_path, device2)
-    
-    # 使用 eos_token 作为 pad_token
-    tokenizer1.pad_token = tokenizer1.eos_token
-    tokenizer2.pad_token = tokenizer2.eos_token
-    tokenizer1.padding_side = "right"   # 使用EOS时,向右填充
-    tokenizer2.padding_side = "right"
-
-    prompts = []
-    padding_max_length = 0
-    with jsonlines.open(data_file_path) as reader:
-        for obj in reader:
-            prompt = obj.get('prompt')
-            prompt_length = tokenizer1(prompt, return_tensors='pt').input_ids.shape[1]
-            padding_max_length = max(padding_max_length, prompt_length)
-
-    # 读取数据文件
-    with jsonlines.open(data_file_path) as reader:
-        for obj in reader:
-            task_id = obj.get('task_id')
-            task_number = int(task_id.split('/')[-1])
-            prompt = obj.get('prompt')
-            print(f"Task ID: {task_id}")
-            # print(f"Prompt: \n{prompt}")
-            prompts.append(prompt)
-            if task_number == 30:
-                break
-
-    # 获取所有 prompts 的输入张量，并进行填充
-    inputs_model1 = tokenizer1(prompts, return_tensors='pt', padding='max_length', max_length=padding_max_length).to(device1)
-    inputs_model2 = tokenizer2(prompts, return_tensors='pt', padding='max_length', max_length=padding_max_length).to(device2)
 
     # 获取隐藏层输出
-    hidden_states_model1 = tokens_get_hidden_states(model1, inputs_model1, device1)
-    hidden_states_model2 = tokens_get_hidden_states(model2, inputs_model2, device2)
+    hidden_states_model1 = load_hidden_states(model1_path)
+    hidden_states_model2 = load_hidden_states(model2_path)
 
     # 获取模型的总层数并计算每一层的CCA相关性得分
     num_layers = len(hidden_states_model1)
@@ -147,9 +101,9 @@ def main(model1_path, model2_path, data_file_path, device1, device2):
         acts1 = acts1.T # convert to neurons by datapoints
         acts2 = acts2.T
 
-        u, s, vh, transformed_a, transformed_b = cca_decomp(acts1, acts2, device1)
-        cal_mean_cca = mean_cca_corr(s)
-        print(f"\tcal_mean_cca: {cal_mean_cca}")
+        # u, s, vh, transformed_a, transformed_b = cca_decomp(acts1, acts2, device1)
+        # cal_mean_cca = mean_cca_corr(s)
+        # print(f"\tcal_mean_cca: {cal_mean_cca}")
 
         acts1_numpy = acts1.cpu().numpy()
         acts2_numpy = acts2.cpu().numpy()
@@ -158,18 +112,13 @@ def main(model1_path, model2_path, data_file_path, device1, device2):
         calculate_cca(acts1_numpy, acts2_numpy, i)
 
 if __name__ == "__main__":
-    # 指定GPU设备
-    device_model1 = torch.device("cuda:2")
-    device_model2 = torch.device("cuda:3")
 
     # 模型和数据路径
-    model_7b = "/newdisk/public/wws/text-generation-webui/models/codeLlama-7b"
-    model_7b_Python = "/newdisk/public/wws/text-generation-webui/models/codeLlama-7b-Python"
+    pt_model_7b = "/newdisk/public/wws/simMeasures/pt_file/JavaScript_hsm1_batch_19.pt"
+    pt_model_7b_Python = "/newdisk/public/wws/simMeasures/pt_file/JavaScript_hsm2_batch_19.pt"
     
-    data_file = "/newdisk/public/wws/humaneval-x-main/data/js/data/humaneval.jsonl"
-
     # 调用主函数
-    main(model_7b, model_7b_Python, data_file, device_model1, device_model2)
+    main(pt_model_7b, pt_model_7b_Python)
             
 
 
